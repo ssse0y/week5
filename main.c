9998c7e9d345fa6d51c819e581b144ff6733446b
ssse0y <ko527ko@gmail.com>
2022-10-04 16:40:55 +0900

    #2

--- main.c
+++ main.c
@@ -4,18 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
+	int i,a;
+	
 	printf("정수 하나를 입력하시오:");
 	scanf("%d", &i);
 	
-	if(i>0)
-		printf("양수입니다.");
-	
-	else if (i<0)  
-	printf("음수 입니다.");
-	
-	else printf(" 0 입니다.");
-	
-	 
+	if(i<0)
+		a=-1*i;  
+		
+		printf("절대값은 %i 입니다.",a);
+
 	return 0;
 }
