57e32ceb67a93fe2a65b8e2ce9a6274e84d83e7d
ssse0y <ko527ko@gmail.com>
2022-10-05 18:14:24 +0900

    #5

--- main.c
+++ main.c
@@ -4,15 +4,37 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-   int sum = 0;
-   int x,i;
    
-   printf("정수를 입력하세요.:");
-   scanf("%d", &x);
+   int a,b,f;
+   char t;
    
-   for (i=0; i<=x; i++)
-   sum= sum+i;
+   printf("정수와 연산자를 입력하세요.:");
+   scanf("%d %c %d",&a,&t,&b);
+   
+   switch(t)
+   {
+      case'+':
+         f=a+b;
+         break;
+         
+      case'-':
+         f=a-b;
+         break;
       
-   printf("결과는 %i 입니다.",sum); 
+      case'/':
+         f=a/b;
+         break;
+      
+      case'*':
+         f=a*b;
+         break;
+         
+      default:
+         f=a%b;
+         break;
+   }
+   
+   printf("=%d \n",f);
+   
    return 0;
 }
