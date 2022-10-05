4b2e6b86f8867c0b5686992a3340b5fbe9b62770
ssse0y <ko527ko@gmail.com>
2022-10-05 18:11:03 +0900

    #4

--- main.c
+++ main.c
@@ -3,21 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main() {
-   char c='0';
-   int num=0;
+int main(int argc, char *argv[]) {
+   int sum = 0;
+   int x,i;
    
-   printf("input a string:");
-   scanf("%c", &c);
-
-   while ((c=getchar())!='\n') //입력문자가 계행문자가 나올 때까지 반복 
-   //반복문을 통해 문자 하나하나 검사 
-   {
-   if(c>='0' && c <='9'){
-   num=num+1;
-   }
-}
-   printf("숫자의 개수는 %i 입니다.\n", num );
-
+   printf("정수를 입력하세요.:");
+   scanf("%d", &x);
+   
+   for (i=0; i<=x; i++)
+   sum= sum+i;
+      
+   printf("결과는 %i 입니다.",sum); 
    return 0;
 }
