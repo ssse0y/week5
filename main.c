4c3af862550cc01a4a633d10933f2c0033046640
ssse0y <ko527ko@gmail.com>
2022-10-05 18:08:43 +0900

    #3

--- main.c
+++ main.c
@@ -3,16 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i,a;
-	
-	printf("정수 하나를 입력하시오:");
-	scanf("%d", &i);
-	
-	if(i<0)
-		a=-1*i;  
-		
-		printf("절대값은 %i 입니다.",a);
+int main() {
+   char c='0';
+   int num=0;
+   
+   printf("input a string:");
+   scanf("%c", &c);
 
-	return 0;
+   while ((c=getchar())!='\n') //입력문자가 계행문자가 나올 때까지 반복 
+   //반복문을 통해 문자 하나하나 검사 
+   {
+   if(c>='0' && c <='9'){
+   num=num+1;
+   }
+}
+   printf("숫자의 개수는 %i 입니다.\n", num );
+
+   return 0;
 }
