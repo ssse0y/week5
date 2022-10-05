a2f5a6df35a242c8528ac533d361525b16dc9759
ssse0y <ko527ko@gmail.com>
2022-10-05 20:17:01 +0900

    #6

--- main.c
+++ main.c
@@ -5,36 +5,32 @@
 
 int main(int argc, char *argv[]) {
    
-   int a,b,f;
-   char t;
-   
-   printf("정수와 연산자를 입력하세요.:");
-   scanf("%d %c %d",&a,&t,&b);
-   
-   switch(t)
+   int an=26, x=0, cnt=0;
+   do
    {
-      case'+':
-         f=a+b;
-         break;
-         
-      case'-':
-         f=a-b;
-         break;
+   	
+   	while(x!=an)
+   	{
+   		printf("숫자를 입력하세요.:");
+		scanf("%d",&x);
+		cnt++;
+		
+		if (x>an){
+		printf("정답보다 큽니다.\n");
+		}
       
-      case'/':
-         f=a/b;
-         break;
-      
-      case'*':
-         f=a*b;
-         break;
-         
-      default:
-         f=a%b;
-         break;
-   }
-   
-   printf("=%d \n",f);
+      else if (x<an)
+      {
+      printf("정답보다 작습니다\n"); 
+	  }
+	  
+      else
+      cnt++;
+      continue;
+      }
+	  }
+   while(x!=an);
+   printf("총 %d번 시도했습니다.\n", cnt);
    
    return 0;
 }
